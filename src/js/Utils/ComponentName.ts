50d45f76a7f43ac4db41b5a98e53d3a56d3408ed
egidio.pignataro <egidio.pignataro@mavriq.biz>
2025-08-14 18:49:15 +0200

    fix type

--- src/js/Utils/ComponentName.ts
+++ src/js/Utils/ComponentName.ts
@@ -1,11 +1,8 @@
 // @ts-ignore jquery is installed from the project, not from the framework
 import $ from "jquery";
 
-const getClassName = (element: HTMLElement): string|null => {
-    return $(element)
-        .attr("class")
-        .split(" ")
-        .find((e) => e.endsWith("-component"));
+const getClassName = (element: HTMLElement): string|undefined => {
+    return $(element)?.attr("class")?.split(" ").find((e) => e.endsWith("-component"));
 }
 
 const transformClassName = (name: string): string => {
@@ -13,18 +10,18 @@ const transformClassName = (name: string): string => {
         .split("-")
         .map((part: string) => part.charAt(0).toUpperCase() + part.slice(1))
         .join("")
-    ;
+        ;
 }
 
 /**
  * Turns html class name 'some-component' into js class name SomeComponent
  * @param element
  */
-export const getComponentName = (element: HTMLElement): string => {
+export const getComponentName = (element: HTMLElement): string|undefined => {
     let classname = getClassName(element);
 
     if (!classname) {
-        return null;
+        return undefined;
     }
 
     return transformClassName(classname);
